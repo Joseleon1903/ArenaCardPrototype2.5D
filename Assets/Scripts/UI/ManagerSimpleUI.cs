b809d2b6317a1d59e8e6dccaebd83d1695f5579f
joseleon1903 <joseleon1903@outlook.com>
2023-06-07 01:45:08 -0400

    update schene workflow

---
+++ Assets/Scripts/UI/ManagerSimpleUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManagerSimpleUI : MonoBehaviour
+{
+
+    public void LoadSceneByName(string name) {
+
+        SceneManager.LoadScene(name);
+    
+    }
+
+   
+}
