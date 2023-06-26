fdc3a934bfdb9ea8cec46c812682acd6bba18d70
joseleon1903 <joseleon1903@outlook.com>
2023-06-26 12:01:45 -0400

    add script general porpuse

---
+++ Assets/Scripts/Logs/ILogger.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Logs
+{
+    public interface ILogger
+    {
+        void WriteLine(string line);
+
+    }
+}
